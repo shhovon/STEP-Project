c9619d415b79d2e67d259e4adda27e64cd32c549
Shovon Roy <Shovon.Roy@pacificjeans.com>
2024-04-20 13:36:09 +0600

    employee view data before submission

--- STEP_DEMO/Controllers/EmployeeController.cs
+++ STEP_DEMO/Controllers/EmployeeController.cs
@@ -1,15 +1,19 @@
-﻿using System;
+﻿using STEP_DEMO.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using STEP_DEMO.Helpers;
+using System.Web.Security;
+
 
 namespace STEP_DEMO.Controllers
 {
     public class EmployeeController : Controller
     {
         // GET: Employee
-
         public ActionResult Index()
         {
             return View();
@@ -21,10 +25,60 @@ namespace STEP_DEMO.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [CustomAuthorize]
+        public ActionResult SpecialFactors(tblSpecial_Factor model)
+        {
+            if (ModelState.IsValid)
+            {
+                Session["SpecialFactors"] = model;
+                return RedirectToAction("TrainingNeed");
+            }
+
+            return View(model);
+        }
+
+
         [CustomAuthorize]
         public ActionResult TrainingNeed()
         {
             return View();
         }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [CustomAuthorize]
+        public ActionResult TrainingNeed(tblTraining_Need model)
+        {
+            if (ModelState.IsValid)
+            {
+                Session["TrainingNeed"] = model;
+                return RedirectToAction("DisplayAllData");
+            }
+
+            return View(model);
+        }
+
+        [CustomAuthorize]
+        public ActionResult DisplayAllData()
+        {
+            var kraKpiOutcomes = Session["UserAddedData"] as List<KraKpiOutcomeModel>;
+            var specialFactors = Session["SpecialFactors"] as tblSpecial_Factor;
+            var trainingNeed = Session["TrainingNeed"] as tblTraining_Need;
+
+            var viewModel = new DisplayAllDataViewModel
+            {
+                KraKpiOutcomes = kraKpiOutcomes,
+                SpecialFactors = specialFactors,
+                TrainingNeed = trainingNeed
+            };
+
+            return View(viewModel);
+        }
+
+
+
     }
 }
---
+++ STEP_DEMO/Models/DisplayAllDataViewModel.cs
@@ -0,0 +1,10 @@
+﻿using STEP_DEMO.Models;
+using System.Collections.Generic;
+
+public class DisplayAllDataViewModel
+{
+    public KraKpiOutcomeModel KraKpiOutcome { get; set; } 
+    public List<KraKpiOutcomeModel> KraKpiOutcomes { get; set; }
+    public tblSpecial_Factor SpecialFactors { get; set; }
+    public tblTraining_Need TrainingNeed { get; set; }
+}
