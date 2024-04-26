8ef94777b9f949af85dafc10c0025a64ca241a97
Shovon Roy <Shovon.Roy@pacificjeans.com>
2024-04-26 12:32:28 +0600

    custom error on for live server

--- STEP_DEMO/Global.asax.cs
+++ STEP_DEMO/Global.asax.cs
@@ -19,7 +19,7 @@ namespace STEP_DEMO
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             GlobalFilters.Filters.Add(new HandleErrorAttribute());
         }
-  /*      protected void Application_Error(object sender, EventArgs e)
+        protected void Application_Error(object sender, EventArgs e)
         {
             var exception = Server.GetLastError();
 
@@ -41,6 +41,6 @@ namespace STEP_DEMO
                      new HttpContextWrapper(Context), routeData);
                 controller.Execute(requestContext);
             }
-        }*/
+        }
     }
 }
