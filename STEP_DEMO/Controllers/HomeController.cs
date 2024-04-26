8b6d723d7259e99f84a9d594b3ddce7796f5808f
Shovon Roy <Shovon.Roy@pacificjeans.com>
2024-04-26 12:11:47 +0600

    outcome data displayed in ascending order

--- STEP_DEMO/Controllers/HomeController.cs
+++ STEP_DEMO/Controllers/HomeController.cs
@@ -120,7 +120,7 @@ namespace STEP_DEMO.Controllers
                         var kraKpiData = (from kra in db.KRAs
                                           join kpi in db.KPIs on kra.KRA_ID equals kpi.KRA_ID
                                           where kra.RegId == regId && !string.IsNullOrEmpty(kra.KRA1) && !string.IsNullOrEmpty(kpi.KPI1)
-                                          orderby kra.KRA_ID descending, kpi.KPI_ID descending
+                                          orderby kra.KRA_ID ascending, kpi.KPI_ID ascending
                                           select new { KRA = kra.KRA1, KPI = kpi.KPI1 }).ToList();
 
                         // Grouping KPIs by KRA
